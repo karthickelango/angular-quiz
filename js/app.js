e9291ebd803dd936bdc2f3526007def39e9b7679
karthickelango <karthik5ive@gmail.com>
2016-10-17 09:51:45 +0530

    Update

--- js/app.js
+++ js/app.js
@@ -8,32 +8,32 @@ myApp.config(["$routeProvider", function ($routeProvider) {
 	}).
 
 	when("/quiz/1", {
-		templateUrl: "./../templates/quiz1.html",
+		templateUrl: "templates/quiz1.html",
 		controller: "quizCtrl"
 	}).
 
 	when("/quiz/2", {
-		templateUrl: "./../templates/quiz2.html",
+		templateUrl: "templates/quiz2.html",
 		controller: "quizCtrl"
 	}).
 
 	when("/quiz/3", {
-		templateUrl: "././../templates/quiz3.html",
+		templateUrl: "templates/quiz3.html",
 		controller: "quizCtrl"
 	}).
 
 	when("/quiz/4", {
-		templateUrl: "./../templates/quiz4.html",
+		templateUrl: "templates/quiz4.html",
 		controller: "quizCtrl"
 	}).
 
 	when("/quiz/5", {
-		templateUrl: "./../templates/quiz5.html",
+		templateUrl: "templates/quiz5.html",
 		controller: "quizCtrl"
 	}).
 
 	when("/result", {
-		templateUrl: "./../templates/result.html",
+		templateUrl: "templates/result.html",
 		controller: "resultCtrl"
 	}).
 
