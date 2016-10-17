0c36a340a28dd41709c8affecd8384c4d6810be6
karthickelango <karthik5ive@gmail.com>
2016-10-17 09:13:09 +0530

    Updated

--- js/app.js
+++ js/app.js
@@ -3,37 +3,37 @@ var myApp = angular.module("myApp", ["ngRoute", "chart.js"]);
 myApp.config(["$routeProvider", function ($routeProvider) {
 	$routeProvider.
 	when("/login", {
-		templateUrl: "../templates/username.html",
+		templateUrl: "./../templates/username.html",
 		controller: "appCtrl"
 	}).
 
 	when("/quiz/1", {
-		templateUrl: "../templates/quiz1.html",
+		templateUrl: "./../templates/quiz1.html",
 		controller: "quizCtrl"
 	}).
 
 	when("/quiz/2", {
-		templateUrl: "../templates/quiz2.html",
+		templateUrl: "./../templates/quiz2.html",
 		controller: "quizCtrl"
 	}).
 
 	when("/quiz/3", {
-		templateUrl: "./../templates/quiz3.html",
+		templateUrl: "././../templates/quiz3.html",
 		controller: "quizCtrl"
 	}).
 
 	when("/quiz/4", {
-		templateUrl: "../templates/quiz4.html",
+		templateUrl: "./../templates/quiz4.html",
 		controller: "quizCtrl"
 	}).
 
 	when("/quiz/5", {
-		templateUrl: "../templates/quiz5.html",
+		templateUrl: "./../templates/quiz5.html",
 		controller: "quizCtrl"
 	}).
 
 	when("/result", {
-		templateUrl: "../templates/result.html",
+		templateUrl: "./../templates/result.html",
 		controller: "resultCtrl"
 	}).
 
